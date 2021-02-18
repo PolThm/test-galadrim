dd23357aaff848ac569ddb46af9fc18da0e8c093
polthm <pol.thomas56@gmail.com>
2021-02-18 11:47:07 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ScreenGaladrim from './src/components/ScreenGaladrim';
+
+const App = () => {
+  return <ScreenGaladrim />;
+};
+
+export default App;
---
+++ src/components/ScreenGaladrim.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const InventoryHome = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Coucou</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default InventoryHome;
