88d2ce8118c6f3a2198868999c4894c46c8def25
polthm <pol.thomas56@gmail.com>
2021-02-18 16:49:07 +0100

    Header done

---
+++ src/assets/img/svgComponents/IconCheckCircle.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Svg, {Path} from 'react-native-svg';
+
+const IconCheckCircle = () => {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={14}
+      height={14}
+      viewBox="0 0 14 14">
+      <Path
+        fill="#009EFF"
+        d="M14 7A7 7 0 110 7a7 7 0 0114 0zm-7.81 3.706l5.194-5.193a.453.453 0 000-.639l-.639-.638a.45.45 0 00-.638 0L5.87 8.47 3.893 6.494a.45.45 0 00-.638 0l-.639.638a.453.453 0 000 .639l2.936 2.935a.45.45 0 00.638 0z"
+        filter="url(#prefix__a)"
+        opacity={0.8}
+      />
+      <Path fill="#009EFF" d="M14 7A7 7 0 110 7a7 7 0 0114 0z" />
+      <Path
+        fill="#FFF"
+        d="M6.283 10.863l4.6-5.398a.53.53 0 000-.664l-.566-.664a.36.36 0 00-.566 0L6 8.54 4.249 6.484a.36.36 0 00-.566 0l-.566.664a.53.53 0 000 .664l2.6 3.05a.36.36 0 00.566 0z"
+      />
+    </Svg>
+  );
+};
+
+export default IconCheckCircle;
--- src/components/ScreenGaladrim.js
+++ src/components/ScreenGaladrim.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import {StyleSheet, Image, Text, View, ImageBackground} from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+import IconCheckCircle from '../assets/img/svgComponents/IconCheckCircle';
 
 const InventoryHome = () => {
   return (
     <ImageBackground
       source={require('../assets/img/bg.jpg')}
-      style={{width: '100%', height: '100%'}}
-    >
+      style={{width: '100%', height: '100%'}}>
       <View style={styles.bgModal} />
       <View style={styles.modal}>
         <View style={styles.modalContainer}>
-          <View style={styles.line} />
+          <View style={styles.modalLine} />
           <Image
-            style={styles.imgProfile}
+            style={styles.modalImg}
             source={require('../assets/img/profile.jpg')}
           />
+          <View style={styles.modalHeader}>
+            <View>
+              <Text style={styles.modalTitle}>Angèle</Text>
+              <View style={styles.modalIconCheck}>
+                <IconCheckCircle style={{elevation: 2}} />
+              </View>
+            </View>
+            <TouchableOpacity style={styles.modalButton}>
+              <Text style={styles.modalTextButton}>S'abonner</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </ImageBackground>
@@ -52,16 +70,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 15,
   },
-  line: {
+  modalLine: {
     backgroundColor: '#D8D8D8',
     width: '20%',
     height: 5,
     borderRadius: 100,
   },
-  imgProfile: {
+  modalImg: {
     marginTop: 20,
     width: '100%',
     borderRadius: 4,
+  },
+  modalHeader: {
+    marginTop: 15,
+    backgroundColor: '#f2faff',
+    width: '100%',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 4,
+  },
+  modalTitle: {
+    fontSize: 50,
+    fontFamily: 'TwCenMT-Bold',
+    letterSpacing: -2.83,
+    color: '#111619',
+  },
+  modalIconCheck: {
+    position: 'absolute',
+    right: -25,
+    top: 15,
+  },
+  modalButton: {
+    marginTop: 15,
+    width: 175,
+    height: 45,
+    borderRadius: 4,
+    backgroundColor: '#009eff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 4,
+  },
+  modalTextButton: {
+    color: 'white',
+    fontFamily: 'TwCenMT',
+    fontSize: 24,
   },
 });
 
